3584e61d87274e81acd4b16c3a5ca7c6f2f3da61
03175121124 <yar85612@Gmail.com>
2023-12-14 22:16:02 -0800

    full

--- lib/IpConfiger.dart
+++ lib/IpConfiger.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:google_fonts/google_fonts.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 class IpConfiger extends StatefulWidget {
   const IpConfiger({super.key});
@@ -56,32 +58,84 @@ class _IpConfigerState extends State<IpConfiger> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 177, 175, 175),
+        elevation: 0,
+        title: Center(
+          child: Text(
+            'IP Address',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+          ),
+        ),
+      ),
       body: Container(
+        color: Colors.cyanAccent,
         width: MediaQuery.of(context).size.width,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            SizedBox(
+              height: 20,
+            ),
             Container(
-              width: 200,
-              height: 50,
-              color: Colors.black,
-              child: Column(children: [
-                Text(
-                  'Ip From Db',
-                  style: TextStyle(),
+              width: MediaQuery.of(context).size.width - 20,
+              height: 170,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40), color: Colors.white),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Ip From Db',
+                                style: TextStyle(
+                                    fontSize: 24,
+                                    fontFamily: GoogleFonts.cabin().fontFamily,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                              SizedBox(
+                                width: 50,
+                              ),
+                             
+                              Text(
+                                ' Current IP',
+                                style: TextStyle(
+                                    fontSize: 24,
+                                    fontFamily: GoogleFonts.dhurjati().fontFamily,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                            ]),
+                      ),
+                      SizedBox(height: 40,),
+                      Row(
+                        children: [
+                          Text(
+                            '$_IpFromDb',
+                            style: TextStyle(
+                                fontFamily: GoogleFonts.inter().fontFamily,fontSize: 15,fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 74,
+                          ),
+                          Text(
+                            '$_IpCurrent',
+                            style: TextStyle(
+                                fontFamily: GoogleFonts.cabin().fontFamily,fontSize: 15,fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-                Text('$_IpFromDb'),
-              ]),
+              ),
             ),
-            SizedBox(
-              height: 50,
-            ),
-            Text('Ip Current'),
-            SizedBox(
-              height: 50,
-            ),
-            Text('$_IpCurrent'),
             SizedBox(
               height: 50,
             ),
@@ -91,10 +145,17 @@ class _IpConfigerState extends State<IpConfiger> {
                 width: 100,
                 height: 100,
                 decoration: BoxDecoration(
-                  color: Colors.blueAccent,
-                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.amberAccent,
+                  borderRadius: BorderRadius.circular(40),
                 ),
-                child: Center(child: Text('data')),
+                child: Center(
+                    child: Text(
+                  'Update',
+                  style: TextStyle(
+                      fontFamily: GoogleFonts.cabin().fontFamily,
+                      fontSize: 19,
+                      fontWeight: FontWeight.w600),
+                )),
               ),
             )
           ],
